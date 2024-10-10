6e6aac8df1073aae5de676c74b510be5d632f657
TuAnhTuan <letuanhtuan7@gmail.com>
2024-10-10 22:59:54 +0700

    Update Film and Language in Model v2

--- hw2/src/main/java/com/wnc/hw2/model/Language.java
+++ hw2/src/main/java/com/wnc/hw2/model/Language.java
@@ -1,7 +1,29 @@
-package com.wnc.hw2;
+package com.wnc.hw2.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 
+import java.time.Instant;
+
+@Getter
+@Setter
 @Entity
+@Table(name = "language")
 public class Language {
-  }
+    @Id
+    @Column(name = "language_id", columnDefinition = "tinyint UNSIGNED not null")
+    private Short id;
+
+    @Column(name = "name", nullable = false, length = 20)
+    private String name;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "last_update", nullable = false)
+    private Instant lastUpdate;
+
+}
