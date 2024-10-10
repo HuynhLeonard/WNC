865296f7060b1659724c4de687d0a09a35b6e386
TuAnhTuan <letuanhtuan7@gmail.com>
2024-10-10 22:44:43 +0700

    Update add FilmRepository and LanguageRepository

---
+++ hw2/src/main/java/com/wnc/hw2/repository/FilmRepository.java
@@ -0,0 +1,8 @@
+package com.wnc.hw2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Long> {
+}
