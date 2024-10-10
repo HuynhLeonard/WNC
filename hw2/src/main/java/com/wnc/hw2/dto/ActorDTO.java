3420f8c51e3c08a4e2ae0fe90ba04f0367cce027
TuAnhTuan <letuanhtuan7@gmail.com>
2024-10-11 00:15:27 +0700

    Update validation for Actor

--- hw2/src/main/java/com/wnc/hw2/dto/ActorDTO.java
+++ hw2/src/main/java/com/wnc/hw2/dto/ActorDTO.java
@@ -1,5 +1,7 @@
 package com.wnc.hw2.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,11 @@ import lombok.Setter;
 @Setter
 @Getter
 public class ActorDTO {
+    @NotBlank(message = "This field cannot be blank")
+    @Size(min = 1, max = 45, message = "First name must be between 1 and 45 characters")
     private String first_name;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 1, max = 45, message = "Last name must be between 2 and 45 characters")
     private String last_name;
 }
