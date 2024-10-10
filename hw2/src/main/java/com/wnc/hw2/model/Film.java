51b00552448a26fb2e5965a9fb8fa0cf00538a45
TuAnhTuan <letuanhtuan7@gmail.com>
2024-10-10 22:57:56 +0700

    Update Film and Language in Model

--- hw2/src/main/java/com/wnc/hw2/model/Film.java
+++ hw2/src/main/java/com/wnc/hw2/model/Film.java
@@ -1,67 +1,7 @@
-package com.wnc.hw2.model;
+package com.wnc.hw2;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
+import jakarta.persistence.Entity;
 
-import java.math.BigDecimal;
-import java.time.Instant;
-
-@Getter
-@Setter
 @Entity
-@Table(name = "film")
 public class Film {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "film_id", columnDefinition = "smallint UNSIGNED not null")
-    private Integer id;
-
-    @Column(name = "title", nullable = false, length = 128)
-    private String title;
-
-    @Lob
-    @Column(name = "description")
-    private String description;
-
-    @Column(name = "release_year")
-    private Integer releaseYear;
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "language_id", nullable = false)
-    private Language language;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "original_language_id")
-    private Language originalLanguage;
-
-    @ColumnDefault("3")
-    @Column(name = "rental_duration", columnDefinition = "tinyint UNSIGNED not null")
-    private Short rentalDuration;
-
-    @ColumnDefault("4.99")
-    @Column(name = "rental_rate", nullable = false, precision = 4, scale = 2)
-    private BigDecimal rentalRate;
-
-    @Column(name = "length", columnDefinition = "smallint UNSIGNED")
-    private Integer length;
-
-    @ColumnDefault("19.99")
-    @Column(name = "replacement_cost", nullable = false, precision = 5, scale = 2)
-    private BigDecimal replacementCost;
-
-    @ColumnDefault("G")
-    @Lob
-    @Column(name = "rating")
-    private String rating;
-
-    @Lob
-    @Column(name = "special_features")
-    private String specialFeatures;
-
-    @ColumnDefault("CURRENT_TIMESTAMP")
-    @Column(name = "last_update", nullable = false)
-    private Instant lastUpdate;
-
-}
+  }
--- hw2/src/main/java/com/wnc/hw2/model/Language.java
+++ hw2/src/main/java/com/wnc/hw2/model/Language.java
@@ -1,26 +1,7 @@
-package com.wnc.hw2.model;
+package com.wnc.hw2;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.Entity;
 
 @Entity
-@Table(name = "language")
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 public class Language {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "language_id")
-    private long id;
-
-    @Column(name = "name", nullable = false, length = 20)
-    private String name;
-
-    @Column(name = "last_update", nullable = false)
-    private String lastUpdate;
-}
+  }
