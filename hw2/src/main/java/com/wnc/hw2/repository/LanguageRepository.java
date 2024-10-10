7018f8715226a8c3488809f28f58d73a228c64d7
TuAnhTuan <letuanhtuan7@gmail.com>
2024-10-10 22:48:32 +0700

    Add Film in Model

--- hw2/src/main/java/com/wnc/hw2/repository/LanguageRepository.java
+++ hw2/src/main/java/com/wnc/hw2/repository/LanguageRepository.java
@@ -1,5 +1,6 @@
 package com.wnc.hw2.repository;
 
+import com.wnc.hw2.model.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
