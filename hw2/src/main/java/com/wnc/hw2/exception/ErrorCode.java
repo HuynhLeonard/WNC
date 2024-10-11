4bcce9d2e4315053a066082e05f7d48b44bdc2ac
LeonardHuynh <thienhuuhuynh.dev@gmail.com>
2024-10-11 14:49:46 +0700

    Update film Error code

--- hw2/src/main/java/com/wnc/hw2/exception/ErrorCode.java
+++ hw2/src/main/java/com/wnc/hw2/exception/ErrorCode.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
-// adding code for film later, start with "2xxx"
 @Getter
 public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
@@ -14,7 +13,21 @@ public enum ErrorCode {
     FIRSTNAME_INVALID(1003, "First name must be between 1 and 45 characters", HttpStatus.BAD_REQUEST),
     LASTNAME_INVALID(1004, "Last name must be between 1 and 45 characters", HttpStatus.BAD_REQUEST),
     ACTOR_NOT_EXISTED(1005, "Actor not existed", HttpStatus.NOT_FOUND),
+
+    // Film-related errors (starting with "2xxx")
+    FILM_NOT_FOUND(2001, "Film not found", HttpStatus.NOT_FOUND),
+    FILM_TITLE_INVALID(2002, "Film title must be between 1 and 255 characters", HttpStatus.BAD_REQUEST),
+    FILM_DESCRIPTION_TOO_LONG(2003, "Film description exceeds allowed length", HttpStatus.BAD_REQUEST),
+    FILM_RELEASE_YEAR_INVALID(2004, "Film release year is invalid", HttpStatus.BAD_REQUEST),
+    FILM_ALREADY_EXISTS(2005, "Film already exists", HttpStatus.BAD_REQUEST),
+    FILM_RENTAL_DURATION_INVALID(2006, "Film rental duration must be between 1 and 30 days", HttpStatus.BAD_REQUEST),
+    FILM_RATING_INVALID(2007, "Film rating is invalid", HttpStatus.BAD_REQUEST),
+    FILM_SPECIAL_FEATURES_INVALID(2008, "Film special features are invalid", HttpStatus.BAD_REQUEST),
+    FILM_RENTAL_RATE_INVALID(2009, "Film rental rate must be a positive value", HttpStatus.BAD_REQUEST),
+    FILM_REPLACEMENT_COST_INVALID(2010, "Film replacement cost must be a positive value", HttpStatus.BAD_REQUEST),
     ;
+
+
 
     ErrorCode(int code, String message, HttpStatusCode statusCode) {
         this.code = code;
