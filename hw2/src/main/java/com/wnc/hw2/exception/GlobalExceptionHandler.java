850ea0e5fbc2c0a17041bb0ceabb3e91bb9533e7
LeonardHuynh <thienhuuhuynh.dev@gmail.com>
2024-10-12 22:16:18 +0700

    Fixing methodnotvalid.class, fixing in response later

--- hw2/src/main/java/com/wnc/hw2/exception/GlobalExceptionHandler.java
+++ hw2/src/main/java/com/wnc/hw2/exception/GlobalExceptionHandler.java
@@ -3,10 +3,12 @@ package com.wnc.hw2.exception;
 import com.wnc.hw2.dto.ApiResponse;
 import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
@@ -36,32 +38,48 @@ public class GlobalExceptionHandler {
     }
 
     // handling for missing data in request.
+//    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+//    ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException exception) {
+//        String enumKey = exception.getFieldError().getDefaultMessage();
+//
+//        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+//        Map<String, Object> attributes = null;
+//        try {
+//            errorCode = ErrorCode.valueOf(enumKey);
+//
+//            var constraintViolation =
+//                    exception.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+//
+//            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+//
+//        } catch (IllegalArgumentException e) {
+//
+//        }
+//
+//        ApiResponse apiResponse = new ApiResponse();
+//
+//        apiResponse.setCode(errorCode.getCode());
+//        apiResponse.setMessage(
+//                Objects.nonNull(attributes)
+//                        ? mapAttribute(errorCode.getMessage(), attributes)
+//                        : errorCode.getMessage());
+//
+//        return ResponseEntity.badRequest().body(apiResponse);
+//    }
+
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    ResponseEntity<ApiResponse> handlingValidation(MethodArgumentNotValidException exception) {
-        String enumKey = exception.getFieldError().getDefaultMessage();
+    ResponseEntity<ApiResponse> handlingValidation2(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
 
-        ErrorCode errorCode = ErrorCode.INVALID_KEY;
-        Map<String, Object> attributes = null;
-        try {
-            errorCode = ErrorCode.valueOf(enumKey);
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
 
-            var constraintViolation =
-                    exception.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
-
-            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
-
-        } catch (IllegalArgumentException e) {
-
-        }
-
-        ApiResponse apiResponse = new ApiResponse();
-
-        apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(
-                Objects.nonNull(attributes)
-                        ? mapAttribute(errorCode.getMessage(), attributes)
-                        : errorCode.getMessage());
-
+        ApiResponse<?> apiResponse = new ApiResponse<>();
+        apiResponse.setCode(404);
+        apiResponse.setMessage(errors.toString());
         return ResponseEntity.badRequest().body(apiResponse);
     }
 
