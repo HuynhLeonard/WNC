2e2183a0ee14e020dee35ae3db91890020c70432
LeonardHuynh <thienhuuhuynh.dev@gmail.com>
2024-10-21 11:25:05 +0700

    Delete HW3

--- hw3/src/main/java/com/wnc/hw3/constant/OpenApiConstant.java
+++
@@ -1,9 +0,0 @@
-package com.wnc.hw3.constant;
-
-public class OpenApiConstant {
-    public static final String DEV_SERVER_URL = "http://localhost:8080";
-    public static final String DEV_SERVER_DESCRIPTION = "Dev Environment";
-    public static final String VERSION = "1.0.0";
-    public static final String TITLE = "API document - HW2";
-    public static final String DESCRIPTION = "WNC API Documentation";
-}
