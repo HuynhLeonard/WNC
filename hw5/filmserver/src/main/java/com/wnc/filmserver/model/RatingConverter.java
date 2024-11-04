f18062bc0593ab8d7422efec6ec68f9ada1127e3
TuAnhTuan <letuanhtuan7@gmail.com>
2024-11-04 22:54:33 +0700

    Adding HW5 FilmServer

---
+++ hw5/filmserver/src/main/java/com/wnc/filmserver/model/RatingConverter.java
@@ -0,0 +1,30 @@
+package com.wnc.filmserver.model;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+import java.util.stream.Stream;
+
+@Converter
+public class RatingConverter implements AttributeConverter<Rating, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Rating rating) {
+        if (rating == null) {
+            return null;
+        }
+        return rating.getCode();
+    }
+
+    @Override
+    public Rating convertToEntityAttribute(String code) {
+        if (code == null) {
+            return null;
+        }
+
+        return Stream.of(Rating.values())
+                .filter(r -> r.getCode().equals(code))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+}
---
+++ hw5/filmserver/src/main/java/com/wnc/filmserver/repository/LanguageRepository.java
@@ -0,0 +1,9 @@
+package com.wnc.filmserver.repository;
+
+import com.wnc.filmserver.model.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<Language, Long> {
+}
